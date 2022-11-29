0fd4f02bba110e6d2e784d649e56e7ed84c38cb5
Ayoub Hiar <ahiar@minsait.com>
2022-11-29 15:52:43 +0100

    Added save paciente endpoint

--- src/main/java/com/grupo/SpringAppEquipo/Controller/PacienteController.java
+++ src/main/java/com/grupo/SpringAppEquipo/Controller/PacienteController.java
@@ -1,7 +1,22 @@
 package com.grupo.SpringAppEquipo.Controller;
 
+import com.grupo.SpringAppEquipo.Service.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class PacienteController {
+
+    @Autowired
+    PacienteService pacienteService;
+
+    @PostMapping("/savePaciente")
+    public ResponseEntity<Object> savePaciente(@RequestBody Map<String, Object> paciente) {
+        return ResponseEntity.ok().body(pacienteService.save(paciente));
+    }
 }
--- src/main/java/com/grupo/SpringAppEquipo/Service/PacienteService.java
+++ src/main/java/com/grupo/SpringAppEquipo/Service/PacienteService.java
@@ -1,7 +1,38 @@
 package com.grupo.SpringAppEquipo.Service;
 
+import com.grupo.SpringAppEquipo.entity.Paciente;
+import com.grupo.SpringAppEquipo.repository.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class PacienteService {
+
+    @Autowired
+    PacienteRepository pacienteRepository;
+
+    public List<Paciente> findAll() {
+        return pacienteRepository.findAll();
+    }
+
+    public Map<String, String> save(Map<String, Object> paciente) {
+        Paciente p = new Paciente();
+        Map<String, String> response = new HashMap<>();
+
+        try {
+            p.setEdad((Integer) paciente.get("edad"));
+            p.setEnfermedad((String) paciente.get("enfermedad"));
+            p.setNombreCompleto((String) paciente.get("NombreCompleto"));
+            pacienteRepository.save(p);
+
+            response.put("status", "usuario guardado con éxito");
+        } catch (Exception e) {
+            response.put("error", e.getMessage());
+        }
+        return response;
+    }
 }
