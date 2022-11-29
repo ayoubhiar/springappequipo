b376747f510bf850b45f2b49f0f8f599a483d475
Ruiz Rubio <aruizr@eservicios.indra.es>
2022-11-29 15:19:32 +0100

    Añadida query para agrupar pacientes por enfermedad

--- src/main/java/com/grupo/SpringAppEquipo/repository/MedicoRepository.java
+++ src/main/java/com/grupo/SpringAppEquipo/repository/MedicoRepository.java
@@ -1,8 +1,12 @@
 package com.grupo.SpringAppEquipo.repository;
 
-import com.grupo.SpringAppEquipo.entity.Medico;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import com.grupo.SpringAppEquipo.entity.Medico;
 
 @Repository
 public interface MedicoRepository extends JpaRepository<Medico, Integer> {
--- src/main/java/com/grupo/SpringAppEquipo/repository/PacienteRepository.java
+++ src/main/java/com/grupo/SpringAppEquipo/repository/PacienteRepository.java
@@ -1,9 +1,16 @@
 package com.grupo.SpringAppEquipo.repository;
 
-import com.grupo.SpringAppEquipo.entity.Paciente;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import com.grupo.SpringAppEquipo.entity.Paciente;
 
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
+	@Query("select p from Paciente p where enfermedad=:enfermedad")
+	List<Paciente> listarPorEnfermedad(@Param("enfermedad") String enfermedad);
 }
