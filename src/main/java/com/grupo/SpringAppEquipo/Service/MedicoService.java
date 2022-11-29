d6f5a875e55c672a1b1db3e95e6e229663f4241c
Ruiz Rubio <aruizr@eservicios.indra.es>
2022-11-29 15:46:42 +0100

    Métodos insertar, modificar y borrar de MedicoService

--- src/main/java/com/grupo/SpringAppEquipo/Service/MedicoService.java
+++ src/main/java/com/grupo/SpringAppEquipo/Service/MedicoService.java
@@ -1,7 +1,33 @@
 package com.grupo.SpringAppEquipo.Service;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.grupo.SpringAppEquipo.entity.Medico;
+import com.grupo.SpringAppEquipo.repository.MedicoRepository;
 
 @Service
 public class MedicoService {
+	@Autowired
+	private MedicoRepository medicoRepository;
+
+	public MedicoService() {
+		super();
+		System.out.println("Creando una instancia de "+this.getClass().getSimpleName());
+	}
+	@Transactional
+	public void insertar(Medico medico) {
+		medicoRepository.save(medico);
+	}
+	@Transactional
+	public void modificar(Medico medico) {
+		medicoRepository.save(medico);
+	}
+	@Transactional
+	public void eliminar(Medico medico) {
+		medicoRepository.delete(medico);
+	}
+	
 }
