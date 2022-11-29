a8d1320b0405b4c3f1f7279ec2820cd47212676f
Ruiz Rubio <aruizr@eservicios.indra.es>
2022-11-29 13:23:37 +0100

    Añadida anotación @Data

--- src/main/java/com/grupo/SpringAppEquipo/entity/Paciente.java
+++ src/main/java/com/grupo/SpringAppEquipo/entity/Paciente.java
@@ -4,9 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+
+import lombok.Data;
 
 @Entity
+@Data
 public class Paciente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
