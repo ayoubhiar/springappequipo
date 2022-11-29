95fc5732a96fa026a8e9e2c2c0d921cbd3e0b378
Ayoub Hiar <ahiar@minsait.com>
2022-11-29 13:45:48 +0100

    Repository interfaces added

---
+++ src/main/java/com/grupo/SpringAppEquipo/repository/ConsultaRepository.java
@@ -0,0 +1,9 @@
+package com.grupo.SpringAppEquipo.repository;
+
+import com.grupo.SpringAppEquipo.entity.Consulta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConsultaRepository extends JpaRepository<Consulta, Integer> {
+}
---
+++ src/main/java/com/grupo/SpringAppEquipo/repository/PacienteRepository.java
@@ -0,0 +1,9 @@
+package com.grupo.SpringAppEquipo.repository;
+
+import com.grupo.SpringAppEquipo.entity.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
+}
