01c6ec5382f4b0b20f7930add6562622002e5260
Ruiz Rubio <aruizr@eservicios.indra.es>
2022-11-29 16:30:09 +0100

    Métodos findMedico, findAllMedio y findMedioByName

--- src/main/java/com/grupo/SpringAppEquipo/Controller/MedicoController.java
+++ src/main/java/com/grupo/SpringAppEquipo/Controller/MedicoController.java
@@ -1,7 +1,42 @@
 package com.grupo.SpringAppEquipo.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.grupo.SpringAppEquipo.Service.MedicoService;
+import com.grupo.SpringAppEquipo.entity.Medico;
+import com.grupo.SpringAppEquipo.repository.MedicoRepository;
 
 @RestController
 public class MedicoController {
+	@Autowired
+	private MedicoService medicoService;
+	@Autowired
+	private MedicoRepository medicoRepository;
+
+	public MedicoController() {
+		super();
+		System.out.println("Creando una instancia de " + this.getClass().getSimpleName());
+	}
+
+	@GetMapping("/findMedico")
+	public ResponseEntity<Object> findMedico(@RequestParam("id") Integer id) {
+		System.out.println("Seleccionar médico con id " + id);
+		Medico medico = medicoRepository.findById(id).orElse(new Medico());
+		return ResponseEntity.ok().body(medico);
+	}
+	@GetMapping("/findAllMedico")
+	public ResponseEntity<Object> findAllMedico() {
+		return ResponseEntity.ok().body( medicoRepository.findAll());
+	}
+	@GetMapping("/findMedicoByName")
+	public ResponseEntity<Object> findMedicoByName(String nombre) {
+		return ResponseEntity.ok().body( medicoRepository.findByName(nombre));
+	}
+
 }
--- src/main/java/com/grupo/SpringAppEquipo/Service/PacienteService.java
+++ src/main/java/com/grupo/SpringAppEquipo/Service/PacienteService.java
@@ -1,13 +1,14 @@
 package com.grupo.SpringAppEquipo.Service;
 
-import com.grupo.SpringAppEquipo.entity.Paciente;
-import com.grupo.SpringAppEquipo.repository.PacienteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.grupo.SpringAppEquipo.entity.Paciente;
+import com.grupo.SpringAppEquipo.repository.PacienteRepository;
 
 @Service
 public class PacienteService {
